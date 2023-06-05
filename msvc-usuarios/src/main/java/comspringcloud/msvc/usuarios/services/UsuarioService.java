f27a0a6c90fde83cd73fe07b31bf09f1582503b5
AlejandroHack1 <xespitia@hotmail.com>
2023-06-05 16:28:07 -0500

    first commit

---
+++ msvc-usuarios/src/main/java/comspringcloud/msvc/usuarios/services/UsuarioService.java
@@ -0,0 +1,21 @@
+package comspringcloud.msvc.usuarios.services;
+
+import comspringcloud.msvc.usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void eliminar(Long id);
+    List<Usuario> listarPorIds(Iterable<Long> ids);
+
+    Optional<Usuario> porEmail(String email);
+
+    Optional<Usuario> porEmail2(String email);
+
+    Boolean porEmail3(String email);
+
+}
